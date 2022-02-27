bc7ce6d3c3251e390856ab4b7d8c631b38ccb5f4
Prakash Gupta <pgupta@netsuite.com>
2022-02-27 10:34:37 -0500

    updated Employee class with constructor, static variables

--- JavaBasics/src/MyPackage/Employee.java
+++ JavaBasics/src/MyPackage/Employee.java
@@ -4,19 +4,40 @@ public class Employee {
 	
 	//State
 	int empId;
-	String name;
+	String employeeName;
 	int age;
 	double salary;
 	float currentExperience, totalExperience;	
+	static int totalEmployees;  // Class/Global variable
+	
+	//Constructor	
+	public Employee(String employeeName) {
+		this.employeeName = employeeName;
+		totalEmployees++; // increment by 1
+	}
+	
+	public Employee(String employeeName, int age, float totalExperience) {
+		this.employeeName = employeeName;
+		this.age = age;
+		this.totalExperience = totalExperience;
+		totalEmployees++; // increment by 1
+	}
+	
+	public static int getTotalEmployees() {
+		return totalEmployees;
+	}
 	
 	public int getEmpId() {
 		return empId;
 	}
 	public String getName() {
-		return name;
+		return employeeName;
 	}
 	public int getAge() {
 		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
 	}
 	public double getSalary() {
 		return salary;
@@ -27,18 +48,22 @@ public class Employee {
 	public float getTotalExperience() {
 		return totalExperience;
 	}
+	public void setTotalExperience(float totalExperience) {
+		this.totalExperience = totalExperience;
+	}
 		
-	//Behaviour
-	
+	//Behaviour	
 	public double calculateSalary(float totalExperience) {
 		return totalExperience*100000;
 	} 
 	
 	public boolean salaryRaiseEligibility() {
+		boolean isEligibleForRaise;
 		if(currentExperience > 1)
-			return true;
+			isEligibleForRaise = true;
 		else
-			return false;
+			isEligibleForRaise = false;
+		return isEligibleForRaise;
 	}
 	
 	void increaseSalaryAnnually() {
@@ -47,8 +72,19 @@ public class Employee {
 	
 	// It should be written outside the class from wherever you are using Employee class
 	public static void main(String[] args) {
-		Employee employee1 = new Employee();
-		Employee employee2 = new Employee();
+		Employee employee1 = new Employee("Employee 1", 25, 5);
+		Employee employee2 = new Employee("Employee 2");
+		employee2.setAge(30);
+		employee2.setTotalExperience(8);
+		
+		System.out.println("Total Employees: "+ Employee.getTotalEmployees());
+		
+		System.out.println("Employee1 name: "+ employee1.getName());
+		System.out.println("Employee2 name: "+ employee2.getName());
+		
+		// calculate their salaries
+		System.out.println("Employee1 salary: "+ employee1.calculateSalary(employee1.getTotalExperience()));
+		System.out.println("Employee2 salary: "+ employee2.calculateSalary(employee2.getTotalExperience()));
 		
 		
 	}
